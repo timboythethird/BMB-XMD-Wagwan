e2f77861cf033007c59bfcebd76d8cdd9e622037
bwbxmd <techibmb@gmail.com>
2025-06-15 16:43:19 +0300

    Update mimi.js

--- bmbtech/mimi.js
+++ bmbtech/mimi.js
@@ -5,12 +5,12 @@ const { format } = require(__dirname + "/../framework/mesfonctions");
 const os = require("os");
 const moment = require("moment-timezone");
 const s = require(__dirname + "/../set");
-const more = String.fromCharCode(8206)
-const readmore = more.repeat(4001)
+const more = String.fromCharCode(8206);
+const readmore = more.repeat(4001);
 
-zokou({ nomCom: "mimi", categorie: "General" }, async (dest, zk, commandeOptions) => {
-    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
-    let { cm } = require(__dirname + "/../framework//zokou");
+zokou({ nomCom: "scan1", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre, prefixe, nomAuteurMessage, mybotpic } = commandeOptions;
+    let { cm } = require(__dirname + "/../framework/zokou");
     var coms = {};
     var mode = "public";
 
@@ -52,15 +52,12 @@ zokou({ nomCom: "mimi", categorie: "General" }, async (dest, zk, commandeOptions
     const lien = mybotpic();
 
     const contextInfo = {
-        externalAdReply: {
-            title: "B.M.B-TECH CHANNEL",
-            body: "Follow for updates",
-            thumbnailUrl: lien,
-            mediaUrl: lien,
-            sourceUrl: "https://whatsapp.com/channel/0029VawO6hgF6sn7k3SuVU3z", // JID ya channel yako
-            mediaType: 1,
-            renderLargerThumbnail: true,
-            showAdAttribution: true
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: "120363382023564830@newsletter",
+            newsletterName: "𝙱.𝙼.𝙱-𝚇𝙼𝙳",
+            serverMessageId: 1
         }
     };
 
@@ -88,6 +85,13 @@ zokou({ nomCom: "mimi", categorie: "General" }, async (dest, zk, commandeOptions
             repondre("🥵 Image error: " + e);
         }
     } else {
-        repondre(infoMsg + menuMsg);
+        try {
+            await zk.sendMessage(dest, {
+                text: infoMsg + menuMsg,
+                contextInfo
+            }, { quoted: ms });
+        } catch (e) {
+            repondre(infoMsg + menuMsg);
+        }
     }
 });
