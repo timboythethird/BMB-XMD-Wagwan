385112452dd29c13ef8a6d4729b18886643aa286
bwbxmd <techibmb@gmail.com>
2025-06-20 00:59:33 +0300

    Update deploy.js

--- bmbtech/deploy.js
+++ bmbtech/deploy.js
@@ -43,7 +43,7 @@ Hello ${nomAuteurMessage},,
          𝐇𝐎𝐖 𝐓𝐎 𝐆𝐄𝐓 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐈𝐃
 ✞ Open this link 👉 https://bmb-tech-pair-site.onrender.com/pair   then tap on pair code.
 ✞ Enter your whatsapp number with the country code e.g;  255767862457 then tap submit 
-✞Queen-m owner , *bmb tech* ,will send you the code immediately.Copy the code and whatsapp will bring a notification 
+✞B.M.B-TECH owner , *bmb tech* ,will send you the code immediately.Copy the code and whatsapp will bring a notification 
 ✞Tap on the notification and paste the code that queen-m owner sent you.
 ✞ After a successful login queen-m owner 🤔 bmb tech will send you a *session id* .some will wonder where the hell is that session ,,,...It is just right at your inbox /dm {your own number for whatsappp} 😂
 ✞Copy the Session id {the one with unreadable codes and long copy it and send it to your deployer or deploy
