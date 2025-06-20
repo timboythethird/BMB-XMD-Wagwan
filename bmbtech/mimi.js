2c0b9c27a21edf8fc24a2b1663b676a599cef79e
bwbxmd <techibmb@gmail.com>
2025-06-20 13:54:14 +0300

    Update mimi.js

--- bmbtech/mimi.js
+++ bmbtech/mimi.js
@@ -1,66 +1,153 @@
-const { zokou } = require("../framework/zokou");
-const moment = require("moment-timezone");
-const { getBuffer } = require("../framework/dl/Function");
-const { default: axios } = require('axios');
+const {
+  zokou
+} = require("../framework/zokou");
+const yts = require("yt-search");
 
-const runtime = function (seconds) { 
- seconds = Number(seconds); 
- var d = Math.floor(seconds / (3600 * 24)); 
- var h = Math.floor((seconds % (3600 * 24)) / 3600); 
- var m = Math.floor((seconds % 3600) / 60); 
- var s = Math.floor(seconds % 60); 
- var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
- var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
- var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
- var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
- return dDisplay + hDisplay + mDisplay + sDisplay; 
- } 
-
-
-zokou({ nomCom: 'time1',
-    desc: 'To check runtime',
-    Categorie: 'General',
-    reaction: '⚙️', 
-    fromMe: 'true', 
-
-
-  },
-  async (dest, zk, commandeOptions) => {
-    const { ms, arg, repondre } = commandeOptions;
-
-                 await repondre(`*_uptime of 
-                 𝐁.𝐌.𝐁-𝐗𝐌𝐃 is: ${runtime(process.uptime())}_*`) 
-
-   
-
-
+zokou({
+  'nomCom': "video",
+  'categorie': "Search",
+  'reaction': '🤳'
+}, async (_0x5949b0, _0x5baae4, _0x3a94eb) => {
+  const {
+    ms: _0x5983ad,
+    repondre: _0x635a46,
+    arg: _0x30ebe9
+  } = _0x3a94eb;
+  if (!_0x30ebe9[0x0]) {
+    _0x635a46("Please insert a song/video name.");
+    return;
   }
-);
+  try {
+    let _0x1ce9a9 = _0x30ebe9.join(" ");
+    const _0x41f7af = await yts(_0x1ce9a9);
+    const _0x4c658e = _0x41f7af.videos;
+    if (_0x4c658e && _0x4c658e.length > 0x0) {
+      const _0x30b168 = _0x4c658e[0x0];
+      const _0x26a944 = {
+        'image': {
+          'url': _0x30b168.thumbnail
+        },
+        'caption': "YOUTUBE SEARCH\n\n ✞𓊈𝗕.𝗠.𝗕-𝗧𝗘𝗖𝗛𓊉 ✞"
+      };
+      await _0x5baae4.sendMessage(_0x5949b0, _0x26a944, {
+        'quoted': _0x5983ad
+      });
+      await _0x5baae4.sendMessage(_0x5949b0, {
+        'video': {
+          'url': _0x30b168.url
+        },
+        'mimetype': "video/mp4"
+      }, {
+        'quoted': _0x5983ad
+      });
+      _0x635a46("Downloaded Successfully✅");
+    } else {
+      _0x635a46("No videos found.");
+    }
+  } catch (_0x3b7436) {
+    console.error("Error from API:", _0x3b7436);
+    _0x635a46("Searching...😪");
+  }
+});
 
+zokou({
+  'nomCom': 'song1',
+  'categorie': "Download",
+  'reaction': '🎺'
+}, async (_0x59ddf5, _0x58c8b7, _0x43867f) => {
+  const {
+    ms: _0x1676d0,
+    repondre: _0x1e8024,
+    arg: _0x2bac41
+  } = _0x43867f;
+  if (!_0x2bac41[0x0]) {
+    _0x1e8024("Please insert a song name.");
+    return;
+  }
+  try {
+    let _0x57159a = _0x2bac41.join(" ");
+    const _0x23e972 = await yts(_0x57159a);
+    const _0x1b10ba = _0x23e972.videos;
+    if (_0x1b10ba && _0x1b10ba.length > 0x0) {
+      const _0x3eec13 = _0x1b10ba[0x0];
+      const _0x369750 = {
+        'image': {
+          'url': _0x3eec13.thumbnail
+        },
+        'caption': "YOUTUBE SEARCH\n\n ✞𓊈𝗕.𝗠.𝗕-𝗧𝗘𝗖𝗛𓊉 ✞"
+      };
+      await _0x58c8b7.sendMessage(_0x59ddf5, _0x369750, {
+        'quoted': _0x1676d0
+      });
+      await _0x58c8b7.sendMessage(_0x59ddf5, {
+        'audio': {
+          'url': 'your_download_url'
+        },
+        'mimetype': "audio/mp4"
+      }, {
+        'quoted': _0x1676d0
+      });
+      _0x1e8024("*Downloaded Successfully ✅*");
+    } else {
+      _0x1e8024("No audio found.");
+    }
+  } catch (_0x43792f) {
+    console.error("Error from API:", _0x43792f);
+    _0x1e8024("An error occurred while searching or downloading the audio.");
+  }
+});
 
-zokou({ nomCom: 'ss',
-    desc: 'screenshots website',
-    Categorie: 'General',
-    reaction: '🎥', 
-    fromMe: 'true', 
-
-},
-  async (dest, zk, commandeOptions) => {
-    const { ms, arg, repondre } = commandeOptions;
-
-    if (!arg || arg.length === 0) return repondre("provide a link...");
-
-         const linkk = arg.join(' ');
-
-
-
-let linkkk = `https://api.screenshotmachine.com/?key=c04d3a&url=${encodeURIComponent(linkk)}&dimension=720x720`;
-
-let res = await getBuffer(linkkk);
-
-await zk.sendMessage(dest, { image: res, caption: '𝑾𝒆𝒃 𝑺𝒄𝒓𝒆𝒆𝒏𝒔𝒉𝒐𝒕 𝒃𝒚 *𝐁.𝐌.𝐁-𝐗𝐌𝐃*'}, { quoted: ms });
-
-
-}
-);
-                             
+zokou({
+  'nomCom': "play1",
+  'categorie': 'Download',
+  'reaction': '🎤'
+}, async (_0x5af0bd, _0x3d387a, _0x1e68f3) => {
+  const {
+    ms: _0x29402c,
+    repondre: _0x4bd959,
+    arg: _0x410d76
+  } = _0x1e68f3;
+  if (!_0x410d76[0x0]) {
+    _0x4bd959("Please insert a song name.");
+    return;
+  }
+  try {
+    let _0x3548af = _0x410d76.join(" ");
+    const _0x4048ad = await yts(_0x3548af);
+    const _0x46b582 = _0x4048ad.videos;
+    if (_0x46b582 && _0x46b582.length > 0x0) {
+      const _0x42b788 = _0x46b582[0x0];
+      const _0x138150 = _0x42b788.url;
+      const _0x549b95 = await fetch("https://api.ssateam.my.id/api/ytdl?urls=" + encodeURIComponent(_0x138150) + "&apiKey=root");
+      const _0x44b9bf = await _0x549b95.json();
+      if (_0x44b9bf.status === 0xc8 && _0x44b9bf.success) {
+        const _0x37c038 = _0x44b9bf.result.download_url;
+        const _0x4301f2 = {
+          'image': {
+            'url': _0x42b788.thumbnail
+          },
+          'caption': "YOUTUBE SEARCH\n\n ✞𓊈𝗕.𝗠.𝗕-𝗧𝗘𝗖𝗛𓊉 ✞"
+        };
+        await _0x3d387a.sendMessage(_0x5af0bd, _0x4301f2, {
+          'quoted': _0x29402c
+        });
+        await _0x3d387a.sendMessage(_0x5af0bd, {
+          'audio': {
+            'url': _0x37c038
+          },
+          'mimetype': "audio/mp4"
+        }, {
+          'quoted': _0x29402c
+        });
+        _0x4bd959("*Downloaded Successfully ✅*");
+      } else {
+        _0x4bd959("Failed to download audio. Please try again later.");
+      }
+    } else {
+      _0x4bd959("No audio found.");
+    }
+  } catch (_0x5d2a11) {
+    console.error("Error from API:", _0x5d2a11);
+    _0x4bd959("An error occurred while searching or downloading the audio.");
+  }
+});
