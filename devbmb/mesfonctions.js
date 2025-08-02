8c2e7a117300b1c6aa202d3639b22518aa57396f
bmb tech <techibmb@gmail.com>
2025-08-02 10:20:20 +0300

    Update mesfonctions.js

--- devbmb/mesfonctions.js
+++ devbmb/mesfonctions.js
@@ -9,7 +9,7 @@ const baileys_1 = require("@whiskeysockets/baileys");
 const fs = require('fs-extra');
 const util = require('util');
 let { listall } = require('./stylish-font');
-/*_________by Mselachui 
+/*_________by B.M.B-TECH
 
 fonction zJson:
 récupère un objet json
