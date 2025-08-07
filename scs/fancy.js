a6f64b6b146dfc29adf99cafac0356b92f16def2
bmb tech <techibmb@gmail.com>
2025-08-07 10:47:08 +0300

    Update fancy.js

--- scs/fancy.js
+++ scs/fancy.js
@@ -1,25 +1,42 @@
 const { bmbtz } = require("../devbmb/bmbtz");
-const fancy = require("../scs/style");
+const fancy = require("../devbmb/function/styles");
 
-bmbtz({ nomCom: "fancy", categorie: "Fun", reaction: "✍️" }, async (dest, zk, commandeOptions) => {
-    const { arg, repondre, prefixe } = commandeOptions;
-    const id = arg[0]?.match(/\d+/)?.join('');
-    const text = arg.slice(1).join(" ");
+bmbtz({
+  nomCom: "fancy",
+  categorie: "Fun",
+  reaction: "✍️",
+  desc: "transform text into fancy style",
+  alias: ['fc']
+}, async (message, reply, options) => {
+  const {
+    arg,
+    repondre,
+    prefixe
+  } = options;
 
-    try {
-        if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 B.M.B-TECH\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('bmb-tech', fancy));
-        }
+  const styleIndex = arg[0]?.match(/\d+/)?.join('');
+  const textToStyle = arg.slice(1).join(" ");
 
-        const selectedStyle = fancy[parseInt(id) - 1];
-        if (selectedStyle) {
-            return await repondre(fancy.apply(selectedStyle, text));
-        } else {
-            return await repondre('_Style introuvable :(_');
-        }
-    } catch (error) {
-        console.error(error);
-        return await repondre('_Une erreur s\'est produite :(_');
+  try {
+    if (!styleIndex || !textToStyle) {
+      return await repondre(
+        "\nExemple : " + prefixe + "fancy 10 Bmb-Tech\n" +
+        String.fromCharCode(0x200e).repeat(4001) +
+        fancy.list("bmb-tech", fancy)
+      );
     }
+
+    const styleFunc = fancy[parseInt(styleIndex) - 1];
+
+    if (styleFunc) {
+      const styledText = fancy.apply(styleFunc, textToStyle);
+      return await repondre(styledText);
+    } else {
+      return await repondre("_Style not found :(_");
+    }
+
+  } catch (error) {
+    console.error(error);
+    return await repondre("_An error occurred :(_");
+  }
 });
-            
